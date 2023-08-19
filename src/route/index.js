8cf1698623757021acee1801ae5a639377fcbb79
OlhaKur <kylovalola55@gmail.com>
2023-08-19 19:09:56 +0300

    product1

--- src/route/index.js
+++ src/route/index.js
@@ -52,7 +52,63 @@ class User {
     }
   }
 }
+// ================================================================
 
+class Product {
+  static #list = []
+  constructor(name, price, description) {
+    this.id = this.uniqueId()
+    this.name = name
+    this.price = price
+    this.description = description
+    this.id = new Date().toISOString()
+  }
+
+  uniqueId() {
+    const min = 10000
+    const max = 99999
+    const uniqueNumber =
+      Math.floor(Math.random() * (max - min + 1)) + min
+    return uniqueNumber
+  }
+
+  static add = (product) => {
+    this.#list.push(product)
+  }
+  static getList = () => this.#list
+
+  static getById = (id) =>
+    this.#list.find((product) => product.id === id)
+
+  static deleteById = (id) => {
+    let index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== 1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, data) => {
+    const product = this.getById(id)
+
+    if (product) {
+      this.update(product, data)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (product, { price }) => {
+    if (price) {
+      product.price = price
+    }
+  }
+}
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -133,6 +189,96 @@ router.post('/user-update', function (req, res) {
 })
 
 // =======================================================================
+// ========================================================
+router.get('/product-create', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const list = Product.getList()
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-create', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-create',
+
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ========================================================
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = new Product(name, price, description)
+
+  Product.add(product)
+
+  console.log(Product.getList())
+
+  res.render('success-info', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'success-info',
+    info: 'Товар створений',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+// =====================================================
+
+router.get('/product-list', function (req, res) {
+  const { name, price, description } = req.query
+
+  const product = new Product(name, price, description)
+
+  Product.add(product)
+
+  console.log(Product.getList())
+
+  res.render('product-list', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-list',
+    info: 'Список товарів',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// =================================================
+router.post('/product-list', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = new Product(name, price, description)
+
+  Product.add(product)
+
+  console.log(Product.getList())
+
+  res.render('product-list', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-list',
+    info: 'Товар створений',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query
+  console.log(typeof id)
+
+  Product.deleteById(Number(id))
+
+  res.render('alert', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'alert',
+    info: 'Успішне виконання дії',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ========================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
